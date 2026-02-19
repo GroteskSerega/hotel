3b86440f2e292a71ae074dba2f53a9f03596118d
Сергей Ночвай <SOBAKA@PAVLOVA.ru>
2026-02-19 07:14:21 +0300

    feature/team-7 Finished step 9 Modified logic for findAll content of Hotel with filter. Updated Response list.

--- src/main/java/com/example/hotel/web/dto/v1/HotelListResponse.java
+++ src/main/java/com/example/hotel/web/dto/v1/HotelListResponse.java
@@ -2,5 +2,11 @@ package com.example.hotel.web.dto.v1;
 
 import java.util.List;
 
-public record HotelListResponse(List<HotelResponse> hotels) {
+public record HotelListResponse(
+        List<HotelResponse> hotels,
+        long totalElements,
+        int totalPages,
+        int pageNumber,
+        int pageSize
+) {
 }
